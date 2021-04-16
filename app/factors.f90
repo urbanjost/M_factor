73e58a886e15703c45a5e032ab17265b4487bd5a
John S. Urban <urbanjost@comcast.net>
2021-04-16 14:54:00 -0400

    update javascript index of html

--- app/factors.f90
+++ app/factors.f90
@@ -197,7 +197,7 @@ help_text=[ CHARACTER(LEN=128) :: &
 '@(#)REPORTING BUGS: http://www.urbanjost.altervista.org/>',&
 '@(#)HOME PAGE:      http://www.urbanjost.altervista.org/index.html>',&
 '@(#)LICENSE:        Public Domain>',&
-'@(#)COMPILED:       Sun, Mar 28th, 2021 2:13:41 PM>',&
+'@(#)COMPILED:       Fri, Apr 16th, 2021 2:53:06 PM>',&
 '']
    WRITE(*,'(a)')(trim(help_text(i)(5:len_trim(help_text(i),kind=kind(1))-1)),i=1,size(help_text))
    stop ! if -version was specified, stop
--- app/gcd.f90
+++ app/gcd.f90
@@ -153,7 +153,7 @@ help_text=[ CHARACTER(LEN=128) :: &
 '@(#)REPORTING BUGS: http://www.urbanjost.altervista.org/>',&
 '@(#)HOME PAGE:      http://www.urbanjost.altervista.org/index.html>',&
 '@(#)UUID:           8f39b1e5-592f-4a22-946f-b8da2aa49633>',&
-'@(#)COMPILED:       Sun, Mar 28th, 2021 2:13:41 PM>',&
+'@(#)COMPILED:       Fri, Apr 16th, 2021 2:53:06 PM>',&
 '']
    WRITE(*,'(a)')(trim(help_text(i)(5:len_trim(help_text(i),kind=kind(1))-1)),i=1,size(help_text))
    stop ! if -version was specified, stop
